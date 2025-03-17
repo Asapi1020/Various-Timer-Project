414c78b60d6df2a958754249ffd5aa0d7c1f6f1c
あさぴっぴ1020 <88693257+Asapi1020@users.noreply.github.com>
2025-03-17 19:30:02 +0900

    Workshopの通知基礎を実装

--- src/infra/DiscordWebhookClient.ts
+++ src/infra/DiscordWebhookClient.ts
@@ -1,12 +1,36 @@
 import type { Discord } from "../domain";
 
 export class DiscordWebhookClient {
-	constructor(private earthquakeWebhook: string) {
+	constructor(
+		private earthquakeWebhook: string,
+		private workshopWebhook: string,
+		private workshopUploadThreadID: string,
+		private workshopUpdateThreadID: string,
+	) {
 		this.earthquakeWebhook = earthquakeWebhook;
+		this.workshopWebhook = workshopWebhook;
 	}
 
 	public async alertEarthquake(payload: Discord.Payload): Promise<boolean> {
 		return await this.sendMessage(this.earthquakeWebhook, payload);
+	}
+
+	public async alertWorkshopUpload(payload: Discord.Payload): Promise<boolean> {
+		return this.alertWorkshop(payload, this.workshopUploadThreadID);
+	}
+
+	public async alertWorkshopUpdate(payload: Discord.Payload): Promise<boolean> {
+		return this.alertWorkshop(payload, this.workshopUpdateThreadID);
+	}
+
+	private async alertWorkshop(
+		payload: Discord.Payload,
+		threadID: string,
+	): Promise<boolean> {
+		return await this.sendMessage(
+			`${this.workshopWebhook}?thread_id=${threadID}`,
+			payload,
+		);
 	}
 
 	private async sendMessage(
